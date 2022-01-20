d1157013c73ac3bd87a4082ff891354053b6aa5d
Nithya Narayanan <n.nithya@pm.me>
2022-01-19 22:50:26 -0800

    async redux.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Import redux stuff
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+// Import reducers
+import { reducer } from './reducers';
 
 import App from './App';
 import './index.css';
 
+// Create store
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
+© 2022 GitHub, Inc.
+Terms
+Privacy
